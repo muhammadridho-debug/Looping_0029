e988d64ce79a2b4a02931993fec6051af1b70ce0
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-30 18:22:38 +0800

    inisialisasi struktur dasar dan pengulangan for

---
+++ looping.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int x;
+
+    // Perulangan dengan FOR
+    // mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i <= 4; i++) {
+        cout << "Teknologi Informasi UNY" << endl;
+    }
+    cout << endl;
+    
+    return 0;
+}
