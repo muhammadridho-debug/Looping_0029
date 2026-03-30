fb282e6d092832b03583d0d2d75eeb8ab54e3ccd
muhammadridho-debug <idomuhammad11@gmail.com>
2026-03-30 18:37:28 +0800

    menambahkan input nilai dan perhitungan rata-rata

--- logika_not.cpp
+++ logika_not.cpp
@@ -5,5 +5,11 @@ int main() {
     float nilM, nilB, rerata;
     string status;
 
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+    rerata = (nilM + nilB) / 2;
     return 0;
 }
